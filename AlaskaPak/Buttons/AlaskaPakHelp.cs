8d652056bb760653c831aea931361130e765a27f
Regan Sarwas <regan_sarwas@nps.gov>
2010-12-08 08:53:01 -0900

    Replaced custom graphics to features button with built in command Replaced custom coordinates tool with builtin GotoXY command Enabled Help function; provided simple help doc.

--- AlaskaPak/Buttons/AlaskaPakHelp.cs
+++ AlaskaPak/Buttons/AlaskaPakHelp.cs
@@ -1,4 +1,9 @@
-﻿namespace NPS.AKRO.ArcGIS
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace NPS.AKRO.ArcGIS
 {
     public class AlaskaPakHelp : ESRI.ArcGIS.Desktop.AddIns.Button
     {
@@ -6,11 +11,30 @@
         {
         }
 
-        protected override void OnClick()
+        private string AssemblyDirectory
         {
-            System.Windows.Forms.MessageBox.Show(this.GetType().Name + " Not Available");
+            get
+            {
+                string codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
         }
 
+        protected override void OnClick()
+        {
+            string file = Path.Combine(AssemblyDirectory, Path.Combine("Help", "help.html"));
+            if (File.Exists(file))
+            {
+                Process.Start(file);
+            }
+            else
+            {
+                MessageBox.Show("Unable to find the help files.\n",
+                    "Internal Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
         protected override void OnUpdate()
         {
             Enabled = true;
