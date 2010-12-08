78798755f629144b980b8f77aae3e17039b402a6
Regan Sarwas <regan_sarwas@nps.gov>
2010-12-07 16:29:30 -0900

    Enabled Graphics To Features by finding the system command

--- AlaskaPak/Buttons/GraphicsToFeatures.cs
+++ AlaskaPak/Buttons/GraphicsToFeatures.cs
@@ -1,4 +1,7 @@
-﻿namespace NPS.AKRO.ArcGIS
+﻿using ESRI.ArcGIS.Framework;
+using ESRI.ArcGIS.esriSystem;
+
+namespace NPS.AKRO.ArcGIS
 {
     public class GraphicsToFeatures : ESRI.ArcGIS.Desktop.AddIns.Button
     {
@@ -8,12 +11,19 @@
 
         protected override void OnClick()
         {
-            System.Windows.Forms.MessageBox.Show(this.GetType().Name + " Not Available");
+            UID uid = new UIDClass();
+            uid.Value = "esriArcMapUI.GraphicsToFeaturesCommand";
+            
+            ICommandItem cmd = ArcMap.Application.Document.CommandBars.Find(uid);
+            if (cmd == null)
+                System.Windows.Forms.MessageBox.Show(this.GetType().Name + " Not Available");
+            else
+                cmd.Execute();
         }
 
         protected override void OnUpdate()
         {
-            Enabled = false;
+            Enabled = true;
         }
     }
 }
