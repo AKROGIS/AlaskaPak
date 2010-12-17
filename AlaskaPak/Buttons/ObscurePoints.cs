24fa946f56aaf583fc89e835ecefc043f5a5f3ba
Regan Sarwas <regan_sarwas@nps.gov>
2010-12-17 12:35:00 -0900

    Implemented a simple configuration setting system, and implemented it for Obscure Points tool

--- AlaskaPak/Buttons/ObscurePoints.cs
+++ AlaskaPak/Buttons/ObscurePoints.cs
@@ -1,19 +1,46 @@
-﻿namespace NPS.AKRO.ArcGIS
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using NPS.AKRO.ArcGIS.Common;
+
+namespace NPS.AKRO.ArcGIS
 {
     public class ObscurePoints : ESRI.ArcGIS.Desktop.AddIns.Button
     {
+        private const string _toolName = "ObscurePoints";
+
         public ObscurePoints()
         {
         }
 
         protected override void OnClick()
         {
-            Common.ArcToolBox.Invoke(@"T:\USER\Regan\AlaskaPak Toolbox", "Alaska Pak", "ObscurePoints");
-        }
+            string toolboxPath = Settings.Get("PathToToolbox");
+            if (toolboxPath == null)
+            {
+                MessageBox.Show("Can't find the Setting 'PathToToolbox'.",
+                    "Internal Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Enabled = false;
+                return;
+            }
+            if (File.Exists(toolboxPath))
+            {
+                string directory = Path.GetDirectoryName(toolboxPath);
+                string toolbox = Path.GetFileNameWithoutExtension(toolboxPath);
+                try
+                {
+                    ArcToolBox.Invoke(directory, toolbox, _toolName);
+                }
+                catch
+                {
+                    MessageBox.Show("Unable to find tool: " + _toolName + Environment.NewLine + "in toolbox: " +  toolboxPath,
+                        "Configuration Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
 
-        protected override void OnUpdate()
-        {
-            Enabled = true;
+            }
+            else
+                MessageBox.Show("Path to Alaska Pak Toolbox is not valid." + Environment.NewLine + toolboxPath,
+                    "Configuration Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
     }
 }
---
+++ AlaskaPak/Common/Settings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace NPS.AKRO.ArcGIS.Common
+{
+    class Settings
+    {
+        public static string Get(string setting)
+        {
+            string datafile = Path.Combine(AssemblyDirectory, setting + ".txt");
+            try
+            {
+                using (var file = File.OpenText(datafile))
+                {
+                    return file.ReadToEnd();
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private static string AssemblyDirectory
+        {
+            get
+            {
+                string codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
+
+
+    }
+}
