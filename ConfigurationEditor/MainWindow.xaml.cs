558e79aa1c306e4084ce3048230b9e7f9d1a2abd
Regan Sarwas <regan_sarwas@nps.gov>
2011-08-08 09:23:06 -0800

    Embeded Ionic's DotNetZip library to read/write addin

--- ConfigurationEditor/MainWindow.xaml.cs
+++ ConfigurationEditor/MainWindow.xaml.cs
@@ -1,27 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using Ionic.Zip;
 
 namespace ConfigurationEditor
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            SetDefaults();
         }
+
+        //provide validation for all three text boxes
+        //provide validation for form
+        //disable save button until form validates
+
+        private void SetDefaults()
+        {
+            //try to find the addin;
+            //what about the others??
+                //programer defautls?
+                //read the addin for defaults?
+        }
+
+        private void BrowseForAddinPath(object sender, RoutedEventArgs e)
+        {
+            string newPath = GetPath("ArcGIS AddIn (.esriAddIn)|*.esriAddIn");
+            if (newPath != null)
+                addinPath.Text = newPath;
+        }
+
+        private void BrowseForThemeManagerPathPath(object sender, RoutedEventArgs e)
+        {
+            string newPath = GetPath("Application (.exe)|*.exe");
+            if (newPath != null)
+                themeManagerPath.Text = newPath;
+        }
+
+        private void BrowseForToolboxPath(object sender, RoutedEventArgs e)
+        {
+            string newPath = GetPath("ArcGIS Toolbox (.tbx)|*.tbx");
+            if (newPath != null)
+                toolboxPath.Text = newPath;
+        }
+
+        private void FixArchive(object sender, RoutedEventArgs e)
+        {
+            UpdateArchive(addinPath.Text, themeManagerPath.Text, toolboxPath.Text);
+            Close();
+        }
+
+        private void Cancel(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        
+        private static void UpdateArchive(string archive, string path1, string path2)
+        {
+            using (var zf = new ZipFile(archive))
+            {
+                zf.UpdateEntry("Install/PathToThemeManager.txt", path1);
+                zf.UpdateEntry("Install/PathToToolbox.txt", path2);
+                zf.Save();
+            }
+        }
+
+        private static string GetPath(string filter)
+        {
+            var dlg = new Microsoft.Win32.OpenFileDialog
+                          {
+                              Filter = filter, 
+                              CheckFileExists = true
+                          };
+
+            return dlg.ShowDialog() == true ? dlg.FileName : null;
+        }
+
     }
 }
