77688d282d8650e782398261db38f268eb4e3a1d
Regan Sarwas <regan_sarwas@nps.gov>
2012-06-18 14:38:34 -0800

    Added new tool to build polygons from control point and azimuth/distance

---
+++ AlaskaPak/Buttons/PointToPolygon2.cs
@@ -0,0 +1,15 @@
+﻿namespace NPS.AKRO.ArcGIS.Buttons
+{
+    public class PointToPolygon2 : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        protected override void OnClick()
+        {
+            AlaskaPak.RunProtected(GetType(), MyClick);
+        }
+
+        private static void MyClick()
+        {
+            Common.ArcToolBox.Invoke(Common.Settings.Get("PathToToolbox"), "PolygonBuilder");
+        }
+    }
+}
