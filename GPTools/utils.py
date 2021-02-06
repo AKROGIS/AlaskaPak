41f920f092b841b847e19d7ee1b17e8d04daa51e
Regan Sarwas <Regan_Sarwas@nps.gov>
2021-02-05 20:08:47 -0900

    Replace utils.py with clean version from Animal Movements

--- GPTools/utils.py
+++ GPTools/utils.py
@@ -1,56 +1,37 @@
+# -*- coding: utf-8 -*-
 """
-Utility functions for use with AlaskaPak Scripts
-
-:Created: 2013-10-24
-
-:Credits:
-  Regan Sarwas, Alaska Region GIS Team, National Park Service
-
-:License:
-  Public Domain
-
-:Disclaimer:
-  This software is provide "as is" and the National Park Service gives
-  no warranty, expressed or implied, as to the accuracy, reliability,
-  or completeness of this software. Although this software has been
-  processed successfully on a computer system at the National Park
-  Service, no warranty expressed or implied is made regarding the
-  functioning of the software on another system or for general or
-  scientific purposes, nor shall the act of distribution constitute any
-  such warranty. This disclaimer applies both to individual use of the
-  software and aggregate use with other software.
-
-Requirements:
-  * Python 2.7+ or 3.x
-  * ArcGIS arcpy module v10.1+ basic (or better) license
+Utility functions for use with ArcGIS 10.1
+Created: 2013-10-24
 """
+
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 import sys
 
 import arcpy
-import arcpy.da
 
 
 def die(msg):
-        arcpy.AddError(msg)
-        print("ERROR: " + str(msg))
-        sys.exit()
+    """Print error message to console and ArcGIS and exit."""
+    arcpy.AddError(msg)
+    print("ERROR: {0}".format(msg))
+    sys.exit()
 
 
 def warn(msg):
-        arcpy.AddWarning(msg)
-        print("Warning: " + str(msg))
+    """Print warning message to console and ArcGIS and exit."""
+    arcpy.AddWarning(msg)
+    print("Warning: {0}".format(msg))
 
 
 def info(msg):
-        arcpy.AddMessage(msg)
-        print("Info: " + str(msg))
+    """Print info message to console and ArcGIS and exit."""
+    arcpy.AddMessage(msg)
+    print("Info: {0}".format(msg))
 
 
 def is_float(something):
-    """Returns True if *something* is a float,
-    or something convertible to a float like '4.2'
-    """
+    """Return True is something is convertible to a floating point number."""
     try:
         float(something)
     except (ValueError, TypeError):
@@ -59,9 +40,7 @@ def is_float(something):
 
 
 def is_int(something):
-    """Returns True if *something* is an int,
-    or something convertible to an int like '42'
-    """
+    """Return True is something is convertible to a integer number."""
     try:
         int(something)
     except (ValueError, TypeError):
@@ -69,26 +48,26 @@ def is_int(something):
     return True
 
 
-def frange(x, y, jump):
+def float_range(start, stop, step=1.0):
     """Return a range of numbers from x to y by jump increments.
-    It is intended to be a floating point version of range().
-    """
+    It is intended to be a floating point version of range()."""
 
-    if jump == 0:
+    if step == 0:
         raise ValueError("jump must be non-zero")
-    if jump > 0:
-        while x < y:
-            yield x
-            x += jump
+    if step > 0:
+        while start < stop:
+            yield start
+            start += step
     else:
-        while x > y:
-            yield x
-            x += jump
+        while start > stop:
+            yield start
+            start += step
 
 
-def get_points(points_feature, sr=None):
-    """Returns a python list of (x,y) pairs"""
-    with arcpy.da.SearchCursor(points_feature, 'SHAPE@XY',
-                               spatial_reference=sr) as searchCursor:
-        points = [row[0] for row in searchCursor]
+def get_points(point_feature, spatial_reference=None):
+    """returns a python list of (x,y) pairs"""
+    with arcpy.da.SearchCursor(
+        point_feature, "SHAPE@XY", spatial_reference=spatial_reference
+    ) as cursor:
+        points = [row[0] for row in cursor]
     return points
