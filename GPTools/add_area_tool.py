d551e256c64468cd5a818c5781aca10470e498fe
Regan Sarwas <Regan_Sarwas@nps.gov>
2021-02-05 19:10:00 -0900

    Add doc string and lint the tool definition

--- GPTools/add_area_tool.py
+++ GPTools/add_area_tool.py
@@ -1,19 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+ArcGIS Tool Classes for a Python Toolbox.
+
+Compatible with Python 2.7 and 3.5+ (ArcGIS 10.x and Pro)
+
+The tool class must meet specific requirements expected by the ArcGIS host.
+See https://pro.arcgis.com/en/pro-app/latest/arcpy/geoprocessing_and_python/
+    defining-a-tool-in-a-python-toolbox.htm
+for details.
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 import arcpy
 
 import add_area
 
+# pylint: disable=invalid-name,no-self-use,unused-argument
+# Framework requirement of the class violate pylint standards.
+
+# pylint: disable=useless-object-inheritance
+# required for Python 2/3 compatibility
+
+
 class AddAreaMultiple(object):
+    """A tool to add an Area column to multiple polygon feature classes."""
+
     def __init__(self):
         self.canRunInBackground = True
 
     def getParameterInfo(self):
+        """Define parameter definitions"""
         features = arcpy.Parameter(
             name="features",
             displayName="Input Features",
             direction="Input",
             datatype="GPFeatureLayer",
             parameterType="Required",
-            multiValue=True)
+            multiValue=True,
+        )
         features.filter.list = ["Polygon"]
 
         field_name = arcpy.Parameter(
@@ -21,7 +46,8 @@ class AddAreaMultiple(object):
             displayName="Field name",
             direction="Input",
             datatype="Field",
-            parameterType="Optional")
+            parameterType="Optional",
+        )
         field_name.value = "Area"
 
         units = arcpy.Parameter(
@@ -29,7 +55,8 @@ class AddAreaMultiple(object):
             displayName="Areal Units",
             direction="Input",
             datatype="GPString",
-            parameterType="Optional")
+            parameterType="Optional",
+        )
         units.filter.list = add_area.valid_units
 
         overwrite = arcpy.Parameter(
@@ -37,37 +64,57 @@ class AddAreaMultiple(object):
             displayName="Overwrite Existing Values",
             direction="Input",
             datatype="GPBoolean",
-            parameterType="Optional")
+            parameterType="Optional",
+        )
 
         parameters = [features, field_name, units, overwrite]
         return parameters
 
+    def isLicensed(self):
+        """Set whether tool is licensed to execute."""
+        return True
+
     def updateParameters(self, parameters):
+        """
+        Modify the values and properties of parameters before internal
+        validation is performed.
+
+        This method is called whenever a parameter has been changed.
+        """
         return
 
     def updateMessages(self, parameters):
+        """
+        Modify the messages created by internal validation for each tool parameter.
+
+        This method is called after internal validation.
+        """
         return
 
     def execute(self, parameters, messages):
+        """Get the parameters and execute the task of the tool."""
         features = parameters[0].value
         units = parameters[1].valueAsText
         fieldname = parameters[2].valueAsText
         overwrite = parameters[3].value
-        add_area.add_area_to_features(features, units,
-                                      fieldname, overwrite)
+        add_area.add_area_to_features(features, units, fieldname, overwrite)
 
 
 class AddAreaSingle(object):
+    """A tool to add an Area column to multiple polygon feature classes."""
+
     def __init__(self):
         self.canRunInBackground = True
 
     def getParameterInfo(self):
+        """Define parameter definitions"""
         feature = arcpy.Parameter(
             name="feature",
             displayName="Input Feature",
             direction="Input",
             datatype="GPFeatureLayer",
-            parameterType="Required")
+            parameterType="Required",
+        )
         feature.filter.list = ["Polygon"]
 
         field_name = arcpy.Parameter(
@@ -75,7 +122,8 @@ class AddAreaSingle(object):
             displayName="Field name",
             direction="Input",
             datatype="Field",
-            parameterType="Optional")
+            parameterType="Optional",
+        )
         field_name.value = "Area"
         field_name.parameterDependencies = [feature.name]
         field_name.filter.list = ["Double"]
@@ -85,7 +133,8 @@ class AddAreaSingle(object):
             displayName="Areal Units",
             direction="Input",
             datatype="GPString",
-            parameterType="Optional")
+            parameterType="Optional",
+        )
         units.filter.list = add_area.valid_units
 
         overwrite = arcpy.Parameter(
@@ -93,33 +142,50 @@ class AddAreaSingle(object):
             displayName="Overwrite Existing Values",
             direction="Input",
             datatype="GPBoolean",
-            parameterType="Optional")
+            parameterType="Optional",
+        )
 
         out_feature = arcpy.Parameter(
             name="out_feature",
             displayName="Output Feature",
             direction="Output",
             datatype="GPFeatureLayer",
-            parameterType="Derived")
+            parameterType="Derived",
+        )
         out_feature.parameterDependencies = [feature.name]
         out_feature.schema.clone = True
 
         parameters = [feature, field_name, units, overwrite, out_feature]
         return parameters
 
+    def isLicensed(self):
+        """Set whether tool is licensed to execute."""
+        return True
+
     def updateParameters(self, parameters):
+        """
+        Modify the values and properties of parameters before internal
+        validation is performed.
+
+        This method is called whenever a parameter has been changed.
+        """
         if parameters[0].altered:
-            parameters[1].value = arcpy.ValidateFieldName(parameters[1].value,
-                                                          parameters[0].value)
-        return
+            parameters[1].value = arcpy.ValidateFieldName(
+                parameters[1].value, parameters[0].value
+            )
 
     def updateMessages(self, parameters):
+        """
+        Modify the messages created by internal validation for each tool parameter.
+
+        This method is called after internal validation.
+        """
         return
 
     def execute(self, parameters, messages):
+        """Get the parameters and execute the task of the tool."""
         feature = parameters[0].valueAsText
         units = parameters[1].valueAsText
         field_name = parameters[2].valueAsText
         overwrite = parameters[3].value
-        add_area.add_area_to_feature(feature, units,
-                                     field_name, overwrite)
+        add_area.add_area_to_feature(feature, units, field_name, overwrite)
