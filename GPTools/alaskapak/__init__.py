f11d52594605f9230566906ba4319d034abd8e88
Regan Sarwas <Regan_Sarwas@nps.gov>
2021-02-08 10:52:17 -0900

    Refactor and publish add_length functions

--- GPTools/alaskapak/__init__.py
+++ GPTools/alaskapak/__init__.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Expose select internal module items as a single module.
+A toolkit of geo-processing tools.
+
+These tools were created for special tasks by Alaska NPS GIS users
+and then generalized.
 """
+# Expose select internal module items as a single module.
 from .add_area import add_area_to_feature, add_area_to_features
 from .add_area import valid_units as valid_area_units
+from .add_length import add_length_to_feature, add_length_to_features
 from .add_length import valid_units as valid_length_units
 from .points_to_polygons import points_to_polygons
--- GPTools/alaskapak/add_length.py
+++ GPTools/alaskapak/add_length.py
@@ -67,14 +67,8 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import arcpy
 
-# TODO: issues:
-# * addfield may alter the name of the field, but the original name is
-# still used for calc field
-# * If coordinate system is unknown, units are assumed to be units
-# requested.
-# * If coordinates are geographic, results are wrong. (the shape_length is also
-# wrong - it uses planar geometry with the spherical coordinates.)
-# * Feature may be locked or un-editable
+from . import utils
+
 
 valid_units = [
     "CENTIMETERS",
@@ -93,14 +87,42 @@ valid_units = [
 ]
 
 
-featureList = arcpy.GetParameterAsText(0)
-fieldName = arcpy.GetParameterAsText(1)
-units = arcpy.GetParameterAsText(2)
+def add_length_to_feature(feature, units, fieldname="Length", overwrite=False):
+    """Add a length attribute to a single polyline or polygon feature class."""
+    # TODO Document parameters in the doc string
 
-for feature in featureList.split(";"):
-    if fieldName not in arcpy.ListFields(feature):
+    # TODO: addfield may alter the name of the field, but the original name is
+    #       still used for calc field
+    # TODO: If coordinate system is unknown, units are assumed to be units requested.
+    # TODO: If coordinates are geographic, results are wrong. (the shape_length is also
+    # wrong - it uses planar geometry with the spherical coordinates.)
+    # TODO: Feature may be locked or un-editable
+
+    if fieldname not in arcpy.ListFields(feature):
         arcpy.AddField_management(
-            feature, fieldName, "Double", "", "", "", "", "NULLABLE", "NON_REQUIRED", ""
+            feature, fieldname, "Double", "", "", "", "", "NULLABLE", "NON_REQUIRED", ""
         )
     length = "!shape.length@{0}!".format(units)
-    arcpy.CalculateField_management(feature, fieldName, length, "PYTHON_9.3", "")
+    arcpy.CalculateField_management(feature, fieldname, length, "PYTHON_9.3", "")
+
+
+def add_length_to_features(features, units, fieldname="Length", overwrite=False):
+    """Add a length attribute to multiple polyline or polygon feature classes."""
+    # TODO Document parameters in the doc string
+
+    for feature in features:
+        utils.info("Adding Length to {0}".format(feature))
+        add_length_to_feature(feature, units, fieldname, overwrite)
+
+
+if __name__ == "__main__":
+    # TODO: this is in a package now, so it can't be called as a script.
+    # TODO: if run as a script for testing, does the `from . import utils` work?
+    feature_list = arcpy.GetParameterAsText(0).split(";")
+    user_units = arcpy.GetParameterAsText(1)
+    # TODO: what if optional parameters are not provided on the command line
+    # TODO: support arcpy command line convention of "#" for None
+    field_name = arcpy.GetParameterAsText(2)
+    overwrite_field = arcpy.GetParameterAsText(3).lower() == "true"
+    # TODO: support parameter(4) output feature class for single or remove option
+    add_length_to_features(feature_list, user_units, field_name, overwrite_field)
