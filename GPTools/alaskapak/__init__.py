067185ecd30b8c9198870b2e9360f6707c433022
Regan Sarwas <Regan_Sarwas@nps.gov>
2021-02-08 10:30:18 -0900

    Cleanup points to polygons and add to package export

--- GPTools/alaskapak/__init__.py
+++ GPTools/alaskapak/__init__.py
@@ -5,3 +5,4 @@ Expose select internal module items as a single module.
 from .add_area import add_area_to_feature, add_area_to_features
 from .add_area import valid_units as valid_area_units
 from .add_length import valid_units as valid_length_units
+from .points_to_polygons import points_to_polygons
