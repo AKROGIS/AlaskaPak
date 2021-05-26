10f943100935954a685e3c8f2be52fdc299e48cf
Regan Sarwas <regan_sarwas@nps.gov>
2021-05-25 21:31:11 -0800

    Flesh out the structure of the Pro AddIn

---
+++ Pro_AddIn/Buttons/AddArea.cs
@@ -0,0 +1,13 @@
+﻿using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+
+namespace AlaskaPak.Buttons
+{
+    internal class AddArea : Button
+    {
+        protected override void OnClick()
+        {
+            MessageBox.Show("Add Area Not Implemented", "Error");
+        }
+    }
+}
